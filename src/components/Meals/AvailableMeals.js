ffd091a75cee19e911b30003ac98c4e205cebe98
dopamine-s <motor55005@gmail.com>
2022-09-21 00:43:55 +0300

    adding Meal Items with inputs

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,19 +1,27 @@
 import { DUMMY_MEALS } from "./dummy-meals";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 import classes from './AvailableMeals.module.css'
 
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map(
     meal => 
-      <li>
-        {meal.name}
-      </li>
+      <MealItem 
+        key={meal.id}
+        id={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
   );
 
   return (
     <section className={classes.meals}>
-      <ul>
-        {mealsList}
-      </ul>
+      <Card>
+        <ul>
+          {mealsList}
+        </ul>
+      </Card>
     </section>
   );
 }
