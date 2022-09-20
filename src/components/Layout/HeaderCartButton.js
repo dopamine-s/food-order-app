17677ee2d97f82f07e26facef664191ec67a2752
dopamine-s <motor55005@gmail.com>
2022-09-20 18:50:32 +0300

    finishing Header component

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -3,12 +3,16 @@ import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
   return (
-    <button className={classes['button']}>
-      <span>
+    <button className={classes.button}>
+      <span className={classes.icon}>
         <CartIcon />
       </span>
-      <span>Cart</span>
-      <span></span>
+      <span>
+        Cart
+      </span>
+      <span className={classes.badge}>
+        3
+      </span>
     </button>
   );
 };
