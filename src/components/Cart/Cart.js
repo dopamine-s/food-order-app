fd91aa54f2b1a512c86c819e8e83a8942c33d074
Dopamine <motor55005@gmail.com>
2022-10-14 16:04:58 +0200

    outputting cart Items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -12,11 +12,22 @@ const Cart = props => {
   const totalAmount = `\u20AC${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
+  const cartItemRemoveHandler = id => {};
+
+  const cartItemAddHandler = item => {};
+
   const cartItems = 
     <ul className={classes['cart-items']}>
       {cartCtx.items.map(
         item =>
-          <CartItem />
+          <CartItem 
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onRemove={cartItemRemoveHandler.bind(null, item.id)}
+            onAdd={cartItemAddHandler.bind(null, item)}
+          />
       )}
     </ul>;
 
