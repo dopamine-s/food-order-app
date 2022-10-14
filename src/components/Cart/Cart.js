15665285353a014e85049e1632b261a89d2b9938
Dopamine <motor55005@gmail.com>
2022-10-15 01:25:39 +0200

    adding editing items in the cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,14 +7,17 @@ import classes from './Cart.module.css';
 
 const Cart = props => {
   const cartCtx = useContext(CartContext);
-  console.log(cartCtx.items);
 
   const totalAmount = `\u20AC${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = id => {};
+  const cartItemRemoveHandler = id => {
+    cartCtx.removeItem(id);
+  };
 
-  const cartItemAddHandler = item => {};
+  const cartItemAddHandler = item => {
+    cartCtx.addItem({...item, amount: 1});
+  };
 
   const cartItems = 
     <ul className={classes['cart-items']}>
