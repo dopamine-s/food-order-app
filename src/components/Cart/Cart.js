a8f4ae8805df89c228d429094b52c7d4259abf0b
Dopamine <motor55005@gmail.com>
2022-10-14 15:21:19 +0200

    Adding useContext in Cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,22 @@
-import { DUMMY_CART_ITEMS } from '../../dummy-data';
+import { useContext } from 'react';
+
+import CartContext from '../../store/cart-context';
 import Modal from '../UI/Modal';
+import CartItem from './CartItem';
 import classes from './Cart.module.css';
 
 const Cart = props => {
+  const cartCtx = useContext(CartContext);
+  console.log(cartCtx.items);
+
+  const totalAmount = `\u20AC${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
   const cartItems = 
     <ul className={classes['cart-items']}>
-      {DUMMY_CART_ITEMS.map(
+      {cartCtx.items.map(
         item =>
-          <li>{item.name}</li>
+          <CartItem />
       )}
     </ul>;
 
@@ -16,14 +25,14 @@ const Cart = props => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button 
           className={classes['button--alt']} onClick={props.onHideCart}>
             Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
