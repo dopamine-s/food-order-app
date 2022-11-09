a6243eb68d7f90591fa922cbbd8acbdb176ec532
Dopamine <motor55005@gmail.com>
2022-11-09 19:10:04 +0100

    fix with Dummy Data

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,59 +1,59 @@
-// import { DUMMY_MEALS } from "../../dummy-data";
-import { useEffect, useState } from "react";
+import { DUMMY_MEALS } from "../../dummy-data";
+// import { useEffect, useState } from "react";
 
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import classes from './AvailableMeals.module.css'
 
 const AvailableMeals = () => {
-  const [meals, setMeals] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [fetchError, setFetchError] = useState(null);
+  // const [meals, setMeals] = useState([]);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [fetchError, setFetchError] = useState(null);
 
-  useEffect(() => {
-    const fetchMeals = async () => {
-      const response = await fetch('https://task-14-http-requests-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+  // useEffect(() => {
+  //   const fetchMeals = async () => {
+  //     const response = await fetch('https://task-14-http-requests-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
       
-      if (!response.ok) {
-        throw new Error('Error occurred');
-      }
+  //     if (!response.ok) {
+  //       throw new Error('Error occurred');
+  //     }
       
-      const responseData = await response.json();
+  //     const responseData = await response.json();
 
-      const loadedMeals = [];
+  //     const loadedMeals = [];
 
-      for (const key in responseData) {
-        loadedMeals.push({
-          id: key,
-          name: responseData[key].name,
-          description: responseData[key].description,
-          price: responseData[key].price,
-        })
-      }
+  //     for (const key in responseData) {
+  //       loadedMeals.push({
+  //         id: key,
+  //         name: responseData[key].name,
+  //         description: responseData[key].description,
+  //         price: responseData[key].price,
+  //       })
+  //     }
 
-      setMeals(loadedMeals);
-      setIsLoading(false);
-    }
+  //     setMeals(loadedMeals);
+  //     setIsLoading(false);
+  //   }
 
-    fetchMeals().catch((error) => {
-      setIsLoading(false);
-      setFetchError(error.message);
-    });
-  },[]);
+  //   fetchMeals().catch((error) => {
+  //     setIsLoading(false);
+  //     setFetchError(error.message);
+  //   });
+  // },[]);
 
-  if (isLoading) {
-    return <section className={classes['meals-loading']}>
-      <p>Loading...</p>
-    </section>
-  };
+  // if (isLoading) {
+  //   return <section className={classes['meals-loading']}>
+  //     <p>Loading...</p>
+  //   </section>
+  // };
 
-  if(fetchError) {
-    return <section className={classes['meals-error']}>
-      <p>{fetchError}</p>
-    </section>
-  }
+  // if(fetchError) {
+  //   return <section className={classes['meals-error']}>
+  //     <p>{fetchError}</p>
+  //   </section>
+  // }
 
-  const mealsList = meals.map(
+  const mealsList = DUMMY_MEALS.map(
     meal => 
       <MealItem 
         key={meal.id}
