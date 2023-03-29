e072182da478e99925fea89503ef637f11252cc8
dopamine-s <motor55005@gmail.com>
2023-03-30 00:00:42 +0200

    refactoring and meals Summary update

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -1,12 +1,11 @@
-import classes from './MealsSummary.module.css'
+import classes from './MealsSummary.module.css';
 
 const MealsSummary = () => {
   return (
     <section className={classes.summary}>
       <h2>Delicious food, delivered to you</h2>
       <p>
-        Choose your favorite meal from our broad selection of available meals
-        and enjoy a delicious lunch or dinner at home.
+        Choose your favorite meal and enjoy a delicious lunch or dinner at home.
       </p>
       <p>
         All our meals are cooked with high-quality ingredients, just-in-time and
